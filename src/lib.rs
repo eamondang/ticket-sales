00475949fe91426bb6f95926a8aaabc406cde975
Eamon Dang <eamondang@gmail.com>
2023-05-22 15:30:00 +0700

    update

--- src/lib.rs
+++ src/lib.rs
@@ -12,14 +12,13 @@ pub use crate::event::*;
 pub struct Contract {
   pub owner_id: AccountId,
   tickets_standard: UnorderedMap<u64, String>,
-  tickets_elite: UnorderedMap<u64, String>,
+  tickets_vip: UnorderedMap<u64, String>,
   tickets_premium: UnorderedMap<u64, String>,
   coupons: UnorderedMap<String, u32>,
-  info: UnorderedMap<String, String>,
-  pub elite_price: Balance,
+  pub vip_price: Balance,
   pub premium_price: Balance,
   pub ticket_standard_saled: u64,
-  pub ticket_elite_saled: u64,
+  pub ticket_vip_saled: u64,
   pub ticket_premium_saled: u64,
   pub buyers: LookupSet<AccountId>,
   pub buyer_ticket_links: UnorderedMap<AccountId, Vec<String>>,
@@ -32,14 +31,13 @@ impl Contract {
     Self {
       owner_id: env::signer_account_id(),
       tickets_standard: UnorderedMap::new(b"tickets_free".to_vec()),
-      tickets_elite: UnorderedMap::new(b"tickets_elite".to_vec()),
+      tickets_vip: UnorderedMap::new(b"tickets_vip".to_vec()),
       tickets_premium: UnorderedMap::new(b"tickets_premium".to_vec()),
       coupons: UnorderedMap::new(b"coupons".to_vec()),
-      info: UnorderedMap::new(b"info".to_vec()),
-      elite_price: 0,
+      vip_price: 0,
       premium_price: 0,
       ticket_standard_saled: 0,
-      ticket_elite_saled: 0,
+      ticket_vip_saled: 0,
       ticket_premium_saled: 0,
       buyers: LookupSet::new(b"buyers".to_vec()),
       buyer_ticket_links: UnorderedMap::new(b"buyer_ticket".to_vec()),
@@ -69,22 +67,27 @@ impl Contract {
     all_tickets
   }
 
-  pub fn add_tickets_elite(&mut self, ticket_links: Vec<String>) {
+  pub fn add_tickets_vip(&mut self, ticket_links: Vec<String>) {
     assert_eq!(env::signer_account_id(), self.owner_id, "Only the owner can add tickets.");
-    let mut key = self.tickets_elite.len();
+    let mut key = self.tickets_vip.len();
 
     for link in ticket_links {
-      self.tickets_elite.insert(&key, &link);
+      self.tickets_vip.insert(&key, &link);
       key += 1;
     }
   }
 
-  pub fn get_all_tickets_elite(&self) -> Vec<(u64, String)> {
+  pub fn total_vip_tickets(&self) -> u64 {
+    assert_eq!(env::signer_account_id(), self.owner_id, "Only the owner can add tickets.");
+    self.tickets_vip.len()
+  }
+
+  pub fn get_all_tickets_vip(&self) -> Vec<(u64, String)> {
     assert_eq!(env::signer_account_id(), self.owner_id, "Only the owner get all tickets.");
     let mut all_tickets = Vec::new();
 
-    for key in 0..self.tickets_elite.len() {
-      if let Some(link) = self.tickets_elite.get(&key) {
+    for key in 0..self.tickets_vip.len() {
+      if let Some(link) = self.tickets_vip.get(&key) {
         all_tickets.push((key, link));
       }
     }
@@ -132,25 +135,19 @@ impl Contract {
     self.coupons.iter().collect()
   }
 
-  pub fn get_all_info(&self) -> Vec<(String, String)> {
-    assert_eq!(env::signer_account_id(), self.owner_id, "Only the owner can get Info.");
-    self.info.iter().collect()
-  }
-
   #[payable]
-  pub fn purchase_elite_ticket(&mut self, email: Option<String>, telephone: Option<String>) -> Promise {
+  pub fn purchase_vip_ticket(&mut self) -> Promise {
     let signer = env::signer_account_id();
-    let key = self.ticket_elite_saled;
-    let price = self.get_elite_price();
+    let key = self.ticket_vip_saled;
+    let price = self.get_vip_price();
 
     assert!(env::attached_deposit() >= price, "Not enough deposit for the ticket.");
     assert!(!self.buyers.contains(&signer), "This wallet has already purchased a ticket.");
-    assert!(self.ticket_elite_saled < 2000, "Ticket sale limit reached.");
-    self.info.insert(&email.unwrap(), &telephone.unwrap());
+    assert!(self.ticket_vip_saled < 2000, "Ticket sale limit reached.");
+    let ticket_link = self.tickets_vip.get(&key).expect("Ticket not available");
 
-    let ticket_link = self.tickets_elite.get(&key).expect("Ticket not available");
-    self.tickets_elite.remove(&key);
-    self.ticket_elite_saled += 1;
+    self.tickets_vip.remove(&key);
+    self.ticket_vip_saled += 1;
     self.buyers.insert(&signer);
 
     // Add the ticket link to the buyer_ticket_links map
@@ -173,28 +170,29 @@ impl Contract {
     Promise::new(self.owner_id.clone()).transfer(price)
   }
 
-  pub fn ticket_premium_price(&mut self, price: Balance, near_price: f32) {
+  pub fn ticket_premium_price(&mut self, price: Balance, near_price: f32) -> Balance {
     assert_eq!(env::signer_account_id(), self.owner_id, "Only the owner can Change Price.");
     let new_price = (price as f32 / near_price) as u128;
-    self.premium_price = new_price;
+    self.premium_price = new_price * 10;
+    new_price
   }
 
   pub fn get_premium_price(&self) -> Balance {
     self.premium_price
   }
 
-  pub fn ticket_elite_price(&mut self, price: Balance, near_price: f32) {
+  pub fn ticket_vip_price(&mut self, price: Balance, near_price: f32) {
     assert_eq!(env::signer_account_id(), self.owner_id, "Only the owner can Change Price.");
     let new_price = (price as f32 / near_price) as u128;
-    self.elite_price = new_price;
+    self.vip_price = new_price;
   }
 
-  pub fn get_elite_price(&self) -> Balance {
-    self.elite_price
+  pub fn get_vip_price(&self) -> Balance {
+    self.vip_price
   }
 
   #[payable]
-  pub fn purchase_premium_ticket(&mut self, email: Option<String>, telephone: Option<String>) -> Promise {
+  pub fn purchase_premium_ticket(&mut self) -> Promise {
     let signer = env::signer_account_id();
     let key = self.ticket_premium_saled;
     let price = self.get_premium_price();
@@ -203,7 +201,6 @@ impl Contract {
     assert!(self.tickets_premium.get(&key).is_some(), "Ticket not available.");
     assert!(self.ticket_premium_saled < 1000, "Ticket sale limit reached.");
 
-    self.info.insert(&email.unwrap(), &telephone.unwrap());
     let ticket_link = self.tickets_premium.get(&key).expect("Ticket not available");
     self.tickets_premium.remove(&key);
     self.ticket_premium_saled += 1;
@@ -235,11 +232,11 @@ impl Contract {
   }
 
   pub fn count_standard(&self) -> u64 {
-    self.ticket_elite_saled
+    self.ticket_vip_saled
   }
 
-  pub fn count_elited(&self) -> u64 {
-    self.ticket_elite_saled
+  pub fn count_vipd(&self) -> u64 {
+    self.ticket_vip_saled
   }
 
   pub fn count_premium(&self) -> u64 {
