0924b5e13df50af14c746dcc627cee4c7df4bb59
Eamon Dang <eamondang@gmail.com>
2023-07-02 02:10:52 +0700

    update: payment version

--- src/lib.rs
+++ src/lib.rs
@@ -58,10 +58,15 @@ impl Contract {
     }
   }
 
-  pub fn ft_on_transfer(&mut self, sender_id: AccountId, amount: Balance, msg: String) -> String {
-    if msg == "VIP" {
+  pub fn ft_on_transfer(&mut self, sender_id: String, amount: String, msg: String) -> String {
+    assert_eq!(
+      env::predecessor_account_id(),
+      "usdt.tether-token.near".parse().unwrap(),
+      "Only token contract can send tokens to this method"
+    );
+    if msg == "VIP".to_string() {
       self.purchase_vip_ticket();
-    } else if msg == "PREMIUM" {
+    } else if msg == "PREMIUM".to_string() {
       self.purchase_premium_ticket();
     }
     "0".to_string()
@@ -69,7 +74,7 @@ impl Contract {
 
   #[private]
   pub fn purchase_premium_ticket(&mut self) {
-    let signer = env::predecessor_account_id();
+    let signer = env::signer_account_id();
     let key = self.ticket_premium_saled;
 
     assert!(self.tickets_premium.get(&key).is_some(), "Ticket not available.");
@@ -98,19 +103,8 @@ impl Contract {
   }
 
   #[private]
-  pub fn query_transfer_callback(&mut self, #[callback_result] call_result: Result<(), PromiseError>) -> bool {
-    // Return whether or not the promise succeeded using the method outlined in external.rs
-    if call_result.is_err() {
-      env::log_str("failed...");
-      false
-    } else {
-      env::log_str("transfer was successful!");
-      true
-    }
-  }
-
-  pub fn purchase_vip_ticket(&mut self) -> Promise {
-    let signer = env::predecessor_account_id();
+  pub fn purchase_vip_ticket(&mut self) {
+    let signer = env::signer_account_id();
     let key = self.ticket_vip_saled;
 
     assert!(self.ticket_vip_saled < 2000, "Ticket sale limit reached.");
@@ -137,11 +131,6 @@ impl Contract {
     };
 
     env::log_str(&purchase_log.to_string());
-
-    ext_ft_contract::ext("usdt.tether-token.near".parse().unwrap())
-      .with_static_gas(Gas(5 * TGAS))
-      .ft_transfer_call("tickets.nearapac.near".parse().unwrap(), U128(10), None, "VIP".to_string())
-      .then(Self::ext(env::current_account_id()).with_static_gas(Gas(5 * TGAS)).query_transfer_callback())
   }
 
   pub fn set_premium_price(&mut self, new_price: u128) -> Balance {
